ef141c57e5bbefdcfabd7c9e69cfcbb49eab6219
Pedja <pmuratov1015@gmail.com>
2022-03-09 23:06:03 -0500

    renamed columns in tables

--- backend/models.py
+++ backend/models.py
@@ -6,18 +6,22 @@ class Player(db.Model):
     __tablename__ = 'players'
     __table_args__ = {'schema': 'nba'}
 
-    playerID = db.Column(db.String(256),primary_key=True)
-    playerName = db.Column(db.String(256))
+    player_id = db.Column(db.String(256),primary_key=True)
+    player_name = db.Column(db.String(256))
+    current_team_id = db.Column(db.String(256))
 
     def __repr__(self) -> str:
-        return f"<Player(playerID='{self.playerID}', playerName='{self.playerName}')"
+        return f"<Player(player_id='{self.player_id}', player_name='{self.player_name}', current_team_id='{self.current_team_id}')"
 
 class Team(db.Model):
     __tablename__ = 'team'
     __table_args__ = {'schema': 'nba'}
     
-    team = db.Column(db.String(256),primary_key=True)
+    team_name = db.Column(db.String(256),unique=True)
+    team_id = db.Column(db.String(256),primary_key=True)
+    team_abb = db.Column(db.String(3),unique=True)
+
 
     def __repr__(self) -> str:
-        return f"Team(team='{self.team}')"
+        return f"Team(team_name='{self.team_name}', team_id={self.team_id}, team_abb={self.team_abb})"
 
