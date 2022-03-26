b10dc3fdb787af953962367802eedc2b5ad385dd
Pedja <pmuratov1015@gmail.com>
2022-03-26 16:45:02 -0400

    added cmd line arguements

--- run_app.sh
+++ run_app.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
+test=0
+while [[ $# -gt 0 ]]; do
+    case $1 in 
+    -h|--host)
+        HOST=$2
+        shift # past arguement
+        shift # past value
+        ;;
+    -p|--port)
+        PORT=$2
+        shift # past arguement
+        shift # past value
+        ;;
+    esac 
+done
+
 NC='\033[0m'
 RED='\033[0;31m'
 BLUE='\033[0;34m'
 GREEN='\033[0;32m'
+
+
 
 function note {
     echo -e "${BLUE} $1 ${NC}"
@@ -21,22 +39,23 @@ function startApp {
 }
 
 function main {
-    host="$1"
-    port="$2"
-
+    echo "$HOST/$PORT"
     service postgresql start
     startApp
-    until $(curl --output /dev/null --silent --head --fail "$host:$port/ready"); do 
+    until $(curl --output /dev/null --silent --head --fail "$HOST:$PORT/ready"); do 
         note ".$NC"
         sleep 3
     
     done
 
     read -p $(note "Press any key to stop the app... \n") -n1 -s
+
     bash shutdown_app.sh
     
 }
 
-main "http://127.0.0.1" "5000"
+
+
+main 
 
 
